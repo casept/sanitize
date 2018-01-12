9cdd2a8db2973899a8f71c9b5eeefc2983b3b613
casept <davids.paskevics@gmail.com>
2018-01-12 19:08:21 +0100

    Remove misleading godoc line

--- sanitize.go
+++ sanitize.go
@@ -12,13 +12,12 @@ type PathNotAbsoluteError struct {
 	path string
 }
 
-// Err serializes the PathNotSaneError.
+// Err serializes the PathNotAbsoluteError.
 func (p PathNotAbsoluteError) Error() string {
 	return fmt.Sprintf("the path %v is not absolute", p.path)
 }
 
 // IsSane checks whether a user-supplied path expands to be outside the provided absolute rootPath.
-// If that's the case the function will return a PathNotSaneError.
 // The provided root path has to be an absolute path in order to avoid surprises during expansion.
 // The function will return a PathNotAbsoluteError otherwise.
 func IsSane(rootPath string, path string) (bool, error) {
